fe21ac2146ac4cc7dadb647d40d33dd5bd2b8a29
vitaliy <sabot05@mail.ru>
2021-07-01 18:17:17 +0700

    first

--- Ex13.5_6.py
+++ Ex13.5_6.py
@@ -2,7 +2,17 @@
 # которая принимает в качестве аргумента строку text
 # и возвращает значение True если указанный текст является палиндромом и False в противном случае.
 def is_palindrome(text):
+    new = text.lower()
+    mylist = list()
+    for i in range(len(new)):
+        if new[i].isalpha():
+            mylist.append(new[i])
+    s = ''.join(mylist)
 
+    if s == s[::-1]:
+        return True
+    else:
+        return False
 
 # считываем данные
 txt = input()
